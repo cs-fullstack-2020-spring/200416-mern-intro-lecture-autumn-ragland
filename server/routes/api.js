68159834f291a1c4ffce23d25e50f03c7c4b4127
autumn-ragland <raglanda998@gmail.com>
2020-04-16 10:31:27 -0500

    backend complete

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,12 +1,21 @@
 let express = require('express');
 let router = express.Router();
+router.use(express.json());
+
+let MovieCollection = require('../models/MovieSchema');
 
 router.get('/', (req, res) => {
-    res.send("GET REQ WORKS")
+    // res.send("GET REQ WORKS")
+    MovieCollection.find({}, (errors, results) => {
+        errors ? res.send(errors) : res.send(results);
+    })
 });
 
 router.post('/', (req, res) => {
-    res.send("POST REQ WORKS")
+    // res.send("POST REQ WORKS")
+    MovieCollection.create(req.body, (errors, results)=> {
+        errors ? res.send(errors) : res.send(results);
+    })
 });
 
 module.exports = router;
